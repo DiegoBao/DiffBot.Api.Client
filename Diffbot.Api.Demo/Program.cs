14be2de011db802479fb3d7accd42937da67a204
Diego Bao <dieg.bao@gmail.com>
2014-01-08 19:46:44 +0100

    Revert "Revert "First version of Article API""
    This reverts commit 8eaaf6aac2aba76ec80d6527d4bc1d5e2ccf6507.

--- Diffbot.Api.Demo/Program.cs
+++ Diffbot.Api.Demo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Diffbot.Api.Client;
+using Diffbot.Api.Client.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,25 +13,90 @@ namespace Diffbot.Api.Demo
     {
         static void Main(string[] args)
         {
-            ApiClient apiClient = new ApiClient("408da6bbb2f6f23fd44f73ee78de2fa8");
+            //ArticleUrl();
+            //ArticleHtmlString();
+            ArticleHtmlStream();
 
-            apiClient.GetArticleAsync("http://blog.xamarin.com", null).ContinueWith(result =>
+            Console.WriteLine("Press any key to finish.");
+            Console.ReadKey();
+        }
+
+        private async static void ArticleHtmlStream()
+        {
+            ApiClient apiClient = new ApiClient();
+
+            try
             {
-                if (!result.IsCompleted)
+
+                using (var file = System.IO.File.OpenRead("demo.html"))
                 {
-                    Console.WriteLine("Oops! something went wrong!");
+                    var result = await apiClient.GetArticleAsync("http://www.diffbot.com/products/automatic/", new string[] { "*" }, null, file);
+
+                    if (result != null)
+                    {
+                        Article a = result;
+                        Console.WriteLine("Title: {0}", a.Title);
+                        Console.WriteLine("Date: {0}", a.Date);
+                        Console.WriteLine("Images: {0}", (a.Images != null) ? a.Images.Count() : 0);
+                    }
                 }
-                else
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private async static void ArticleHtmlString()
+        {
+            ApiClient apiClient = new ApiClient();
+
+            try
+            {
+                using (var file = System.IO.File.OpenText("demo.html"))
                 {
-                    Article a = result.Result;
+                    var html = await file.ReadToEndAsync();
+                    var result = await apiClient.GetArticleAsync("http://www.diffbot.com/products/automatic/", new string[] { "*" }, null, html);
+
+                    if (result != null)
+                    {
+                        Article a = result;
+                        Console.WriteLine("Title: {0}", a.Title);
+                        Console.WriteLine("Date: {0}", a.Date);
+                        Console.WriteLine("Images: {0}", (a.Images != null) ? a.Images.Count() : 0);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        static async void ArticleUrl()
+        {
+            ApiClient apiClient = new ApiClient();
+
+            try
+            {
+                // http://goo.gl/JjqwN
+                // http://www.diffbot.com/dev/docs/article/
+                // http://www.diffbot.com/products/automatic/
+                var result = await apiClient.GetArticleAsync("http://www.diffbot.com/products/automatic/", new string[] { "*" }, null);
+
+                if (result != null)
+                {
+                    Article a = result;
                     Console.WriteLine("Title: {0}", a.Title);
                     Console.WriteLine("Date: {0}", a.Date);
                     Console.WriteLine("Images: {0}", (a.Images != null) ? a.Images.Count() : 0);
                 }
-            });
-
-            Console.WriteLine("Press any key to finish.");
-            Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
