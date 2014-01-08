78f030c3d9a3a71bf946d72b07bfa395015e351e
DiegoBao <diego.bao@gmail.com>
2014-01-08 17:31:55 +0100

    First version of Article API

--- Diffbot.Api.Client/ApiClient.cs
+++ Diffbot.Api.Client/ApiClient.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Diffbot.Api.Client.Model;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +14,134 @@ namespace Diffbot.Api.Client
     {
         private DiffbotCall diffbotCall;
         private string token;
+        private int version;
 
-        public ApiClient(string token)
+        /// <summary>
+        /// Creates an instance of the ApiClient taken the needed parameters from the configuration file.
+        /// </summary>
+        public ApiClient()
+            : this(
+                ConfigurationManager.AppSettings["DiffbotUrl"],
+                ConfigurationManager.AppSettings["DiffbotToken"],
+                ConfigurationManager.AppSettings["DiffbotVersion"])
         {
-            this.token = token;
-            diffbotCall = new DiffbotCall(); // takes base api from config file
         }
-        public ApiClient(string baseApiUrl, string token)
+
+        /// <summary>
+        /// Create an instance of the ApiClient with an specific token. Url and version are taken from the configuration file.
+        /// </summary>
+        /// <param name="token">Diffbot API Token required for using the API.</param>
+        public ApiClient(string token)
+            : this(ConfigurationManager.AppSettings["DiffbotUrl"], token, ConfigurationManager.AppSettings["DiffbotVersion"])
         {
+        }
+
+        /// <summary>
+        /// Create an instance of the ApiClient.
+        /// </summary>
+        /// <param name="baseApiUrl">URL of the Diffbot Api. (ex. http://api.diffbot.com )</param>
+        /// <param name="token">Diffbot API Token required for using the API.</param>
+        /// <param name="version">Vesion of the API</param>
+        public ApiClient(string baseApiUrl, string token, string version)
+        {
+            if (string.IsNullOrWhiteSpace(baseApiUrl))
+            {
+                throw new ArgumentNullException("baseApiUrl");
+            }
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentNullException("token");
+            }
+
+            if (string.IsNullOrWhiteSpace(version))
+            {
+                throw new ArgumentException("Incorrect API version", "version");
+            }
+
             this.token = token;
+            this.version = int.Parse(version);
             diffbotCall = new DiffbotCall(baseApiUrl);
         }
 
-        public async Task<Article> GetArticleAsync(string url, string[] fields)
+        /// <summary>
+        /// Calls the Article API. 
+        /// </summary>
+        /// <param name="url">Article URL to process.</param>
+        /// <param name="fields">Array with the fields that will be returned by the API.</param>
+        /// <returns>Returns information about the primary article content on the submitted page.</returns>
+        public async Task<Article> GetArticleAsync(string url, string[] fields, Dictionary<string, string> optionalParameters)
         {
             try
             {
-                string result = await diffbotCall.ApiCallAsync(url, this.token, "article", fields, 2);
+                JObject result = await diffbotCall.ApiGetAsync(url, this.token, "article", fields, this.version, optionalParameters);
 
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<Article>(result);
+                Article article = Newtonsoft.Json.JsonConvert.DeserializeObject<Article>(result.ToString());
+                article.Properties = new Dictionary<string, JToken>();
+                foreach (var property in ((dynamic)result))
+                {
+                    if (!Article.PropertyNames.Any(p => p == property.Name.ToString()))
+                    {
+                        article.Properties.Add(property.Name, property.Value);
+                    }
+                }
+
+                return article;
+            }
+            catch (Exception ex)
+            {
+                throw ex; //TODO: better exception handler
+            }
+        }
+
+        /// <summary>
+        /// Calls the Article API submitting the html to analyse directly.
+        /// </summary>
+        /// <param name="url">Article URL to process.</param>
+        /// <param name="fields">Array with the fields that will be returned by the API.</param>
+        /// <param name="html">Html to be analysed instead of calling the URL. The url parameter is still required.</param>
+        /// <returns>Returns information about the primary article content on the submitted page.</returns>
+        public async Task<Article> GetArticleAsync(string url, string[] fields, Dictionary<string, string> optionalParameters, string html)
+        {
+            try
+            {
+                JObject result = await diffbotCall.ApiPostAsync(url, this.token, "article", fields, this.version, optionalParameters, html, "text/html");
+
+                Article article = Newtonsoft.Json.JsonConvert.DeserializeObject<Article>(result.ToString());
+                article.Properties = new Dictionary<string, JToken>();
+                foreach (var property in ((dynamic)result))
+                {
+                    if (!Article.PropertyNames.Any(p => p == property.Name.ToString()))
+                    {
+                        article.Properties.Add(property.Name, property.Value);
+                    }
+                }
+
+                return article;
+            }
+            catch (Exception ex)
+            {
+                throw ex; //TODO: better exception handler
+            }
+        }
+
+        /// <summary>
+        /// Calls the Article API  submitting the html to analyse directly. 
+        /// </summary>
+        /// <param name="url">Article URL to process.</param>
+        /// <param name="fields">Array with the fields that will be returned by the API.</param>
+        /// <param name="htmlStream">Stream with the html to be analysed instead of calling the URL. The url parameter is still required.</param>
+        /// <returns>Returns information about the primary article content on the submitted page.</returns>
+        public async Task<Article> GetArticleAsync(string url, string[] fields, Dictionary<string, string> optionalParameters, Stream htmlStream)
+        {
+            try
+            {
+                byte[] buffer = new byte[htmlStream.Length];
+                await htmlStream.ReadAsync(buffer, 0, buffer.Length);
+
+                string html = System.Text.UTF8Encoding.UTF8.GetString(buffer);
+
+                return await GetArticleAsync(url, fields, optionalParameters, html);
             }
             catch (Exception ex)
             {
--- Diffbot.Api.Client/DiffbotCall.cs
+++ Diffbot.Api.Client/DiffbotCall.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -9,19 +11,21 @@ namespace Diffbot.Api.Client
 {
     internal class DiffbotCall
     {
+        
         HttpClient httpClient;
-        public DiffbotCall()
-            : this("http://api.diffbot.com") // Should be in configuration but for demo purposes it's hardcoded
-        {
-        }
 
-        public DiffbotCall(string baseApiUrl)
+        public DiffbotCall(string baseApiUrl) // http://api.diffbot.com
         {
+            if (string.IsNullOrWhiteSpace(baseApiUrl))
+            {
+                throw new ArgumentNullException("baseApiUrl");
+            }
+
             httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(baseApiUrl);
         }
 
-        public async Task<string> ApiCallAsync(string url, string token, string api, string[] fields, int version)
+        public async Task<JObject> ApiGetAsync(string url, string token, string api, string[] fields, int version, Dictionary<string, string> optionalParameters)
         {
             #region Argument Validation
             if (string.IsNullOrWhiteSpace(url))
@@ -50,21 +54,90 @@ namespace Diffbot.Api.Client
             }
             #endregion
 
+            StringBuilder apiUrl = CreateUrl(url, token, api, fields, version, optionalParameters);
+            
+            var result = await httpClient.GetStringAsync(apiUrl.ToString());
+
+            return JObject.Parse(result);
+        }
+
+        private static StringBuilder CreateUrl(string url, string token, string api, string[] fields, int version, Dictionary<string, string> optionalParameters)
+        {
             StringBuilder apiUrl = new StringBuilder();
             apiUrl.AppendFormat("v{0}/{1}?token={2}", version, api, token);
             if (fields != null && fields.Length > 0)
             {
                 apiUrl.AppendFormat("&fields={0}", string.Join(",", fields));
             }
+            if (optionalParameters != null && optionalParameters.Count > 0)
+            {
+                foreach (var k in optionalParameters)
+                {
+                    apiUrl.AppendFormat("&{0}={1}", k.Key, k.Value);
+                }
+            }
             apiUrl.AppendFormat("&url={0}", url);
-
-            return await httpClient.GetStringAsync(apiUrl.ToString());            
+            return apiUrl;
         }
 
         private bool IsValidApi(string api)
         {
             // TODO: rest of the apis
             return api == "article";
+        }
+
+        public async Task<JObject> ApiPostAsync(string url, string token, string api, string[] fields, int version, Dictionary<string, string> optionalParameters, string html, string contentType)
+        {
+            #region Argument Validation
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentNullException("token");
+            }
+
+            if (string.IsNullOrWhiteSpace(api))
+            {
+                throw new ArgumentNullException("api");
+            }
+            else if (!IsValidApi(api))
+            {
+                throw new ArgumentException("Invalid value for parameter 'api'.");
+            }
+
+            if (string.IsNullOrWhiteSpace(html))
+            {
+                throw new ArgumentNullException("html");
+            }
+
+            if (string.IsNullOrWhiteSpace(contentType))
+            {
+                throw new ArgumentNullException("contentType");
+            }
+
+            // TODO: Validate this is a valid check
+            if (version < 2)
+            {
+                throw new ArgumentException("Invalid api version");
+            }
+            #endregion
+
+            StringBuilder apiUrl = CreateUrl(url, token, api, fields, version, optionalParameters);
+
+            var formatter = new System.Net.Http.Formatting.JsonMediaTypeFormatter();
+            var result = await httpClient.PostAsync<string>(apiUrl.ToString(), html, formatter);
+                        
+            if (result.IsSuccessStatusCode)
+            {
+                return JObject.Parse(await result.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                throw new HttpRequestException(result.ReasonPhrase);
+            }            
         }
     }
 }
