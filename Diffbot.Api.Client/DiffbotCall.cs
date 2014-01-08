8eaaf6aac2aba76ec80d6527d4bc1d5e2ccf6507
Diego Bao <dieg.bao@gmail.com>
2014-01-08 19:46:26 +0100

    Revert "First version of Article API"
    This reverts commit 78f030c3d9a3a71bf946d72b07bfa395015e351e.

--- Diffbot.Api.Client/DiffbotCall.cs
+++ Diffbot.Api.Client/DiffbotCall.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -11,21 +9,19 @@ namespace Diffbot.Api.Client
 {
     internal class DiffbotCall
     {
-        
         HttpClient httpClient;
-
-        public DiffbotCall(string baseApiUrl) // http://api.diffbot.com
+        public DiffbotCall()
+            : this("http://api.diffbot.com") // Should be in configuration but for demo purposes it's hardcoded
         {
-            if (string.IsNullOrWhiteSpace(baseApiUrl))
-            {
-                throw new ArgumentNullException("baseApiUrl");
-            }
+        }
 
+        public DiffbotCall(string baseApiUrl)
+        {
             httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(baseApiUrl);
         }
 
-        public async Task<JObject> ApiGetAsync(string url, string token, string api, string[] fields, int version, Dictionary<string, string> optionalParameters)
+        public async Task<string> ApiCallAsync(string url, string token, string api, string[] fields, int version)
         {
             #region Argument Validation
             if (string.IsNullOrWhiteSpace(url))
@@ -54,90 +50,21 @@ namespace Diffbot.Api.Client
             }
             #endregion
 
-            StringBuilder apiUrl = CreateUrl(url, token, api, fields, version, optionalParameters);
-            
-            var result = await httpClient.GetStringAsync(apiUrl.ToString());
-
-            return JObject.Parse(result);
-        }
-
-        private static StringBuilder CreateUrl(string url, string token, string api, string[] fields, int version, Dictionary<string, string> optionalParameters)
-        {
             StringBuilder apiUrl = new StringBuilder();
             apiUrl.AppendFormat("v{0}/{1}?token={2}", version, api, token);
             if (fields != null && fields.Length > 0)
             {
                 apiUrl.AppendFormat("&fields={0}", string.Join(",", fields));
             }
-            if (optionalParameters != null && optionalParameters.Count > 0)
-            {
-                foreach (var k in optionalParameters)
-                {
-                    apiUrl.AppendFormat("&{0}={1}", k.Key, k.Value);
-                }
-            }
             apiUrl.AppendFormat("&url={0}", url);
-            return apiUrl;
+
+            return await httpClient.GetStringAsync(apiUrl.ToString());            
         }
 
         private bool IsValidApi(string api)
         {
             // TODO: rest of the apis
             return api == "article";
-        }
-
-        public async Task<JObject> ApiPostAsync(string url, string token, string api, string[] fields, int version, Dictionary<string, string> optionalParameters, string html, string contentType)
-        {
-            #region Argument Validation
-            if (string.IsNullOrWhiteSpace(url))
-            {
-                throw new ArgumentNullException("url");
-            }
-
-            if (string.IsNullOrWhiteSpace(token))
-            {
-                throw new ArgumentNullException("token");
-            }
-
-            if (string.IsNullOrWhiteSpace(api))
-            {
-                throw new ArgumentNullException("api");
-            }
-            else if (!IsValidApi(api))
-            {
-                throw new ArgumentException("Invalid value for parameter 'api'.");
-            }
-
-            if (string.IsNullOrWhiteSpace(html))
-            {
-                throw new ArgumentNullException("html");
-            }
-
-            if (string.IsNullOrWhiteSpace(contentType))
-            {
-                throw new ArgumentNullException("contentType");
-            }
-
-            // TODO: Validate this is a valid check
-            if (version < 2)
-            {
-                throw new ArgumentException("Invalid api version");
-            }
-            #endregion
-
-            StringBuilder apiUrl = CreateUrl(url, token, api, fields, version, optionalParameters);
-
-            var formatter = new System.Net.Http.Formatting.JsonMediaTypeFormatter();
-            var result = await httpClient.PostAsync<string>(apiUrl.ToString(), html, formatter);
-                        
-            if (result.IsSuccessStatusCode)
-            {
-                return JObject.Parse(await result.Content.ReadAsStringAsync());
-            }
-            else
-            {
-                throw new HttpRequestException(result.ReasonPhrase);
-            }            
         }
     }
 }
--- Diffbot.Api.Demo/Program.cs
+++ Diffbot.Api.Demo/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Diffbot.Api.Client;
-using Diffbot.Api.Client.Model;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,90 +11,25 @@ namespace Diffbot.Api.Demo
     {
         static void Main(string[] args)
         {
-            //ArticleUrl();
-            //ArticleHtmlString();
-            ArticleHtmlStream();
+            ApiClient apiClient = new ApiClient("408da6bbb2f6f23fd44f73ee78de2fa8");
 
-            Console.WriteLine("Press any key to finish.");
-            Console.ReadKey();
-        }
-
-        private async static void ArticleHtmlStream()
-        {
-            ApiClient apiClient = new ApiClient();
-
-            try
+            apiClient.GetArticleAsync("http://blog.xamarin.com", null).ContinueWith(result =>
             {
-
-                using (var file = System.IO.File.OpenRead("demo.html"))
+                if (!result.IsCompleted)
                 {
-                    var result = await apiClient.GetArticleAsync("http://www.diffbot.com/products/automatic/", new string[] { "*" }, null, file);
-
-                    if (result != null)
-                    {
-                        Article a = result;
-                        Console.WriteLine("Title: {0}", a.Title);
-                        Console.WriteLine("Date: {0}", a.Date);
-                        Console.WriteLine("Images: {0}", (a.Images != null) ? a.Images.Count() : 0);
-                    }
+                    Console.WriteLine("Oops! something went wrong!");
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
-        private async static void ArticleHtmlString()
-        {
-            ApiClient apiClient = new ApiClient();
-
-            try
-            {
-                using (var file = System.IO.File.OpenText("demo.html"))
+                else
                 {
-                    var html = await file.ReadToEndAsync();
-                    var result = await apiClient.GetArticleAsync("http://www.diffbot.com/products/automatic/", new string[] { "*" }, null, html);
-
-                    if (result != null)
-                    {
-                        Article a = result;
-                        Console.WriteLine("Title: {0}", a.Title);
-                        Console.WriteLine("Date: {0}", a.Date);
-                        Console.WriteLine("Images: {0}", (a.Images != null) ? a.Images.Count() : 0);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-        }
-
-        static async void ArticleUrl()
-        {
-            ApiClient apiClient = new ApiClient();
-
-            try
-            {
-                // http://goo.gl/JjqwN
-                // http://www.diffbot.com/dev/docs/article/
-                // http://www.diffbot.com/products/automatic/
-                var result = await apiClient.GetArticleAsync("http://www.diffbot.com/products/automatic/", new string[] { "*" }, null);
-
-                if (result != null)
-                {
-                    Article a = result;
+                    Article a = result.Result;
                     Console.WriteLine("Title: {0}", a.Title);
                     Console.WriteLine("Date: {0}", a.Date);
                     Console.WriteLine("Images: {0}", (a.Images != null) ? a.Images.Count() : 0);
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+            });
+
+            Console.WriteLine("Press any key to finish.");
+            Console.ReadKey();
         }
     }
 }
